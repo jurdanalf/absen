6dc060d52f70d6b6030569dc164e6b8416c71603
jurdanalf <jurdanalfirdauz@gmail.com>
2022-09-22 12:21:30 +0700

    adding member

--- index.php
+++ index.php
@@ -4,21 +4,213 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MCA | Home</title>
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <?php include 'inc/nav.php'; ?>
     <main>
         <div class="container">
-            <div class="gp">
-                <div class="card">
-                    <div class="gambar">
-                        <img src="img/aziz.JPG" alt="Sokheh Abdul Aziz">
+            <div id="pgm">
+                <h3>REMAJA USIA PGM</h3>
+                <div id="card-pgm">
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-abdillah.JPG" alt="Abdillah Rahman">
+                        </div>
+                        <div class="detail-pgm">
+                        <h4>Abdillah Rahman</h4>
+                        <table>
+                            <tr>
+                                <td><p>Kelompok</p></td>
+                                <td><p>5</p></td>
+                            </tr>
+                            <tr>
+                                <td><p>Umur</p></td>
+                                <td><p>23</p></td>
+                            </tr>
+                        </table>
+                        </div>
                     </div>
-                    <div class="detail-gp">
-                        <h4>Sokheh Abdul Aziz</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, cum!</p>
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-aisah.JPG" alt="Nurul Aisah">
+                        </div>
+                        <div class="detail-pgm">
+                            <h4>Nurul Aisah</h4>
+                            <table>
+                                <tr>
+                                    <td><p>Kelompok</p></td>
+                                    <td><p>1</p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>Umur</p></td>
+                                    <td><p>21</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-aji.JPG" alt="Ahmad Aji">
+                        </div>
+                        <div class="detail-pgm">
+                            <h4>Ahmad Aji</h4>
+                            <table>
+                                <tr>
+                                    <td><p>Kelompok</p></td>
+                                    <td><p>5</p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>Umur</p></td>
+                                    <td><p>22</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-alfa.JPG" alt="Alfaizuna Aulia Ulloh">
+                        </div>
+                        <div class="detail-pgm">
+                            <h4>Alfaizuna Aulia Ulloh</h4>
+                            <table>
+                                <tr>
+                                    <td><p>Kelompok</p></td>
+                                    <td><p>3</p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>Umur</p></td>
+                                    <td><p>27</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-dea.JPG" alt="Andrea">
+                        </div>
+                        <div class="detail-pgm">
+                            <h4>Andrea Fitriani S.</h4>
+                            <table>
+                                <tr>
+                                    <td><p>Kelompok</p></td>
+                                    <td><p>3</p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>Umur</p></td>
+                                    <td><p>21</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-dewi.JPG" alt="Dewi Masfufah">
+                        </div>
+                        <div class="detail-pgm">
+                            <h4>Dewi Masfufah</h4>
+                            <table>
+                                <tr>
+                                    <td><p>Kelompok</p></td>
+                                    <td><p>2</p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>Umur</p></td>
+                                    <td><p>26</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-dina.JPG" alt="Dina Hanifa">
+                        </div>
+                        <div class="detail-pgm">
+                            <h4>Dina Hanifa</h4>
+                            <table>
+                                <tr>
+                                    <td><p>Kelompok</p></td>
+                                    <td><p>1</p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>Umur</p></td>
+                                    <td><p>26</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-yuda.JPG" alt="M. Anggi Sukma Yudha">
+                        </div>
+                        <div class="detail-pgm">
+                            <h4>M. Anggi Sukma Yudha</h4>
+                            <table>
+                                <tr>
+                                    <td><p>Kelompok</p></td>
+                                    <td><p>5</p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>Umur</p></td>
+                                    <td><p>28</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-krisna.JPG" alt="Krisna Putra Perdana">
+                        </div>
+                        <div class="detail-pgm">
+                            <h4>Krisna Putra Perdana</h4>
+                            <table>
+                                <tr>
+                                    <td><p>Kelompok</p></td>
+                                    <td><p>5</p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>Umur</p></td>
+                                    <td><p>27</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-tirza.JPG" alt="Tirza Ayu Adyni">
+                        </div>
+                        <div class="detail-pgm">
+                            <h4>Tirza Ayu Adyni</h4>
+                            <table>
+                                <tr>
+                                    <td><p>Kelompok</p></td>
+                                    <td><p>3</p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>Umur</p></td>
+                                    <td><p>24</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="gambar">
+                            <img src="img/c-aziz.JPG" alt="Sokheh Abdul Aziz">
+                        </div>
+                        <div class="detail-pgm">
+                            <h4>Sokheh Abdul Aziz</h4>
+                            <table>
+                                <tr>
+                                    <td><p>Kelompok</p></td>
+                                    <td><p>1</p></td>
+                                </tr>
+                                <tr>
+                                    <td><p>Umur</p></td>
+                                    <td><p>23</p></td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
